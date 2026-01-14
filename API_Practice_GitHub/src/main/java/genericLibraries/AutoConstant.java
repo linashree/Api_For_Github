dd7270af77153934c94f4d4fdbeaf3e4f4b992c4
linashree <109854756+linashree@users.noreply.github.com>
2026-01-14 13:22:53 +0530

    Update AutoConstant.java

--- API_Practice_GitHub/src/main/java/genericLibraries/AutoConstant.java
+++ API_Practice_GitHub/src/main/java/genericLibraries/AutoConstant.java
@@ -2,9 +2,10 @@ package genericLibraries;
 
 public interface AutoConstant {
     // API Endpoints
+    //REST API endpoints for repositories - GitHub Docs
     String getOperation = "/repos/{owner}/{repo}";
     String postOperation = "/user/repos";
-    String patchOperation = "/repos/{owner}/{repo}";
+    String patchOperation = "/repos/{owner}/{repo}"..;
     String deleteOperation = "/repos/{owner}/{repo}";
 
     // Property file path
